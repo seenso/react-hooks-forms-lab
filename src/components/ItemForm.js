4ef5e8ee6e2ed34d9f810d13536f4acee613b5f0
seenso <>
2021-11-30 14:11:03 -0900

    Working but not controlled components. Going to work on making it controlled

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,17 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { v4 as uuid } from "uuid";
 
-function ItemForm(props) {
+//a controlled form is a form that derives its input values from state
+
+function ItemForm({onItemFormSubmit}) {
+  const [name, setName] = useState("Lettuce");
+  const [category, setCategory] = useState("Produce");
+
+  function handleNameChange(e) {
+    setName(e.target.value);
+    console.log("handleNameChange value", e.target.value);
+  }
+
+  function handleCategoryClick(e) {
+    setCategory(e.target.value);
+    console.log("handleNameChange value", e.target.value);
+  }
+
+  function submitNewItem(e) {
+    e.preventDefault(); //should always do this onSubmit
+    onItemFormSubmit({
+      id: uuid(),
+      name: name,
+      category: category
+    })
+  }
+
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={submitNewItem}>
       <label>
         Name:
-        <input type="text" name="name" />
+        <input type="text" name="name" onChange={handleNameChange}/>
       </label>
 
       <label>
         Category:
-        <select name="category">
+        <select name="category" onClick={handleCategoryClick}>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -3,23 +3,35 @@ import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
 
-function ShoppingList({ items }) {
+function ShoppingList({ items, onItemFormSubmit }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [searchTerm, setSearchTerm] = useState("");
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
-  const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
+  function handleSearchChange(e){
+    setSearchTerm(e.target.value);
+  }
 
+  const itemsToDisplay = items.filter((item) => {
+    if (selectedCategory === "All") {
+      //return a new arr with all items. It's why we return true for the .filter() method to return everything
+      return true;
+    };
+    //return a new arr with items in selectedCategory
     return item.category === selectedCategory;
-  });
+  }).filter((item) => item.name.toLowerCase().startsWith(searchTerm.toLowerCase()));
+  //^ 2nd .filter() is filtering the new array from the 1st .filter() **doesn't matter which .filter() comes first, it seems like**
+  //making sure case doesn't matter with .toLowerCase()
+  //we can also use .includes to search a phrase in the whole item and not the beginning (using below's code for the second .filter() option)
+  // .filter((item) => item.name.includes(searchTerm));
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <ItemForm onItemFormSubmit={onItemFormSubmit}/>
+      <Filter onCategoryChange={handleCategoryChange} onSearchChange={handleSearchChange}/>
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
